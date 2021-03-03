6e466e794ca39a272ae08852a75f5c2574c43994
ivanamalesevic <ivanamalesevic147@gmail.com>
2021-03-03 20:18:30 +0100

    Done tree component filtering

--- museum-art/src/components/tree/Tree.js
+++ museum-art/src/components/tree/Tree.js
@@ -7,15 +7,20 @@ import {
   RadioGroup,
   FormControlLabel,
   TextField,
-  InputAdornment,
+  Dialog,
+  DialogContent,
 } from "@material-ui/core";
+import { TreeItem, TreeView } from "@material-ui/lab";
 import { makeStyles } from "@material-ui/core/styles";
-import { SearchIcon } from "@material-ui/icons";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import ChevronRightIcon from "@material-ui/icons/ChevronRight";
+// import { SearchIcon } from "@material-ui/icons";
 
 const Tree = () => {
   const [collection, setCollection] = useState({});
   const [typeFilter, setTypeFilter] = useState("all");
   const [searchName, setSearchName] = useState("");
+  const [loadingData, setLoadingData] = useState(true);
 
   const useStyles = makeStyles((theme) => ({
     searchField: {
@@ -39,11 +44,13 @@ const Tree = () => {
           if (response.status === 200) {
             localStorage.setItem("collection", JSON.stringify(response.data));
             setCollection(response.data);
+            setLoadingData(false);
           }
         })
         .catch((err) => console.error("Unable to get collection!"));
     } else {
       setCollection(col);
+      setLoadingData(false);
     }
   };
 
@@ -51,43 +58,84 @@ const Tree = () => {
     getCollection();
   }, []);
 
+  if (loadingData) {
+    return (
+      <Dialog open={loadingData}>
+        <DialogContent>Loading data...</DialogContent>
+      </Dialog>
+    );
+  }
+
+  const displayData = () => {
+    return collection.collection.map((col) => {
+      return (
+        <TreeItem key={col.id} nodeId={col.id} label={col.name}>
+          {col.collection.map((item) => {
+            return (typeFilter === "all" || typeFilter === item.type) && (searchName === "" || item.name.toLowerCase().includes(searchName.toLowerCase())) ? (
+              <TreeItem
+                key={item.id}
+                nodeId={item.id}
+                label={item.name}
+              ></TreeItem>
+            ) : (
+              <></>
+            );
+          })}
+        </TreeItem>
+      );
+    });
+  };
+
   return (
     <Grid item xs={6}>
-      <FormControl component="fieldset" className={classes.radios}>
-        <RadioGroup
-          row
-          aria-label="type"
-          name="type"
-          value={typeFilter}
-          onChange={(e) => setTypeFilter(e.target.value)}
-        >
-          <FormControlLabel value="all" control={<Radio />} label="All" />
-          <FormControlLabel
-            value="painting"
-            control={<Radio />}
-            label="Painting"
-          />
-          <FormControlLabel
-            value="potteries"
-            control={<Radio />}
-            label="Potteries"
-          />
-        </RadioGroup>
-      </FormControl>
-      <TextField
-        className={classes.searchField}
-        id="outlined-search"
-        type="search"
-        variant="outlined"
-        value={searchName}
-        InputProps={{
-          startAdornment: (
-            <InputAdornment position="end">
-              <SearchIcon />
-            </InputAdornment>
-          ),
-        }}
-      />
+      <div>
+        <FormControl component="fieldset" className={classes.radios}>
+          <RadioGroup
+            row
+            aria-label="type"
+            name="type"
+            value={typeFilter}
+            onChange={(e) => setTypeFilter(e.target.value)}
+          >
+            <FormControlLabel value="all" control={<Radio />} label="All" />
+            <FormControlLabel
+              value="painting"
+              control={<Radio />}
+              label="Painting"
+            />
+            <FormControlLabel
+              value="potery"
+              control={<Radio />}
+              label="Potteries"
+            />
+          </RadioGroup>
+        </FormControl>
+        <TextField
+          className={classes.searchField}
+          id="outlined-search"
+          type="search"
+          variant="outlined"
+          value={searchName}
+          onChange={(e) => {setSearchName(e.target.value)}}
+          // InputProps={{
+          //   startAdornment: (
+          //     <InputAdornment position="end">
+          //       <SearchIcon />
+          //     </InputAdornment>
+          //   ),
+          // }}
+        />
+      </div>
+      <br />
+      <br />
+      <TreeView
+        defaultCollapseIcon={<ExpandMoreIcon />}
+        defaultExpandIcon={<ChevronRightIcon />}
+      >
+        <TreeItem nodeId={collection.id} label={collection.name}>
+          {displayData()}
+        </TreeItem>
+      </TreeView>
     </Grid>
   );
 };
