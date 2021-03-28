b7a6ad3ed4c6f7f6e4d03bc09315bf3f11661b49
ivanamalesevic <ivanamalesevic147@gmail.com>
2021-03-28 19:27:35 +0200

    Started tree component

--- museum-art-angular/src/app/app.module.ts
+++ museum-art-angular/src/app/app.module.ts
@@ -2,13 +2,31 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LayoutComponent } from './layout/layout.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatGridListModule} from '@angular/material/grid-list';
+import { TreeComponent } from './tree/tree.component';
+import {MatRadioModule} from '@angular/material/radio';
+import {MatInputModule} from '@angular/material/input';
+import {MatTreeModule} from '@angular/material/tree';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LayoutComponent,
+    TreeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule,
+    BrowserAnimationsModule,
+    MatGridListModule, 
+    MatRadioModule,
+    MatInputModule, 
+    MatTreeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
